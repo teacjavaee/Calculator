f66e45d8eb2547e60229b1b580e5539ff47ade8c
Teac_j <teacjavaee@gmail.com>
2015-10-27 23:11:19 +0300

    Записал в comboBox правильные значения. подцепил observablelist

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,5 +1,23 @@
 package controllers;
 
-public class Controller {
-    //скоро все будет
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ComboBox <String> comboBox;
+
+    ObservableList<String> list = FXCollections.observableArrayList("0.8","1.0","1.15","1.25");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        comboBox.setItems(list);
+    }
 }
