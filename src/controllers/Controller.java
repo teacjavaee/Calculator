944d9036286e793b1e88edcdb97332a9b37bbf8d
Teac_j <teacjavaee@gmail.com>
2015-10-29 21:22:32 +0300

    Немного допилил

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -4,7 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,12 +12,46 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    private ComboBox <String> comboBox;
+    private TextField textFieldPower;
 
-    ObservableList<String> list = FXCollections.observableArrayList("0.8","1.0","1.15","1.25");
+    @FXML
+    private TextField textFieldLine;
+
+    @FXML
+    private CheckBox checkBoxLAP;
+
+    @FXML
+    private CheckBox checkBoxKTP;
+
+    @FXML
+    private CheckBox checkBoxPS;
+
+    @FXML
+    private Label labelnoNDS;
+
+    @FXML
+    private Label labelNDS;
+
+    @FXML
+    private Label labelFull;
+
+    @FXML
+    private Button buttonPDF;
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    ObservableList<String> list = FXCollections.observableArrayList("0.8", "1.0", "1.15", "1.25");
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        textFieldPower.setTooltip(new Tooltip("Мощность вводится в кВт, в качестве разделителя использовать точку"));
+       // textFieldPower.setStyle("-fx-font-weight:bold;-fx-font-size:10pt;-fx-font-family:Georgia"); если шрифт определен в FXML то здесь он переопределяеся и распространяется на весь текс поля.
         comboBox.setItems(list);
+        textFieldLine.setVisible(false);
+        if (checkBoxLAP.isSelected()) {
+            textFieldLine.setVisible(true);
+        }
+
     }
 }
